09bb55069eba393c1c305b162064587c8b27e6b8
Daniel Azocar <danielazocarvelasquezdev@gmail.com>
2020-05-18 23:37:40 -0400

    AddItem component

---
+++ src/components/additem/AddItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./AddItem.scss";
+
+const AddItem = () => {
+  return (
+    <form>
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" />
+      <label htmlFor="price">Price</label>
+      <input type="number" id="price" />
+      <label htmlFor="cost">Cost</label>
+      <input type="number" id="cost" />
+      <label htmlFor="units">Units</label>
+      <input type="number" id="units" />
+      <input type="button" value="Create!" />
+    </form>
+  );
+};
+
+export default AddItem;
