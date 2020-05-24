4b61ae392736c831c8385e71c33fc12e2780b700
Daniel Azocar <danielazocarvelasquezdev@gmail.com>
2020-05-24 16:29:54 -0400

    initial context setup

---
+++ src/context/item/ItemState.js
@@ -0,0 +1,14 @@
+// Importamos react, createContext y useReducer
+import React, { createContext, useReducer } from "react";
+// import de uuid para crear ids aleatorias a los items
+import { v4 as uuid } from "uuid";
+
+const ItemState = ({ children }) => {
+  // Creamos el estado inicial
+  const initialState = {
+    items: [],
+  };
+  return <div></div>;
+};
+
+export default ItemState;
