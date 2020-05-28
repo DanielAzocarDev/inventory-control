59391a6d44e0ad1ee3127bfd99bf7589bb05954d
Daniel Azocar <danielazocarvelasquezdev@gmail.com>
2020-05-27 22:22:14 -0400

    About page content & styles

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyCKfaNfItB6Mu3Sx7duzf0hk03Ri4a7A5s",
-  authDomain: "sells-project.firebaseapp.com",
-  databaseURL: "https://sells-project.firebaseio.com",
-  projectId: "sells-project",
-  storageBucket: "sells-project.appspot.com",
-  messagingSenderId: "533518915234",
-  appId: "1:533518915234:web:48847f2a32c53b82aae9fb",
-};
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import "./About.scss";
+
 const About = () => {
-  return <div>About!</div>;
+  return (
+    <div className="about-container">
+      <h2>About this App</h2>
+      <p>
+        This app is a basic inventory system based on React, React Router,
+        Context API, localStorage and SASS. You can create, read, update and
+        delete the items that you create through the form. While making this app
+        I learn how to setup and manage a centralized state with Context API and
+        it communicated with all the app's components. I hope you like it, stay
+        tuned for upcoming updates that come with new features!
+      </p>
+      <h3>Technologies</h3>
+      <ul>
+        <li>React</li>
+        <li>React Router</li>
+        <li>Context API</li>
+        <li>UUID</li>
+        <li>localStorage</li>
+        <li>SASS</li>
+      </ul>
+    </div>
+  );
 };
 
 export default About;
